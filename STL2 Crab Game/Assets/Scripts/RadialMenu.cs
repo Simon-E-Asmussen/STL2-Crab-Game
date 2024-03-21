6d6c25a2130a2ce7f96c91ecb2173ddf53b470dc
Mathias <maths20@student.sdu.dk>
2024-03-22 00:14:24 +0100

    Can open and close radial menu
    - Using DOTween asset for juice
    - Open and Close functionality
    - Added Juice for menu

--- STL2 Crab Game/Assets/Scripts/RadialMenu.cs
+++ STL2 Crab Game/Assets/Scripts/RadialMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class RadialMenu : MonoBehaviour
 {
@@ -33,11 +34,39 @@ public class RadialMenu : MonoBehaviour
 
     public void Open()
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 8; i++)
         {
             AddEntry("Button" + i.ToString(), icons[i]);
         }
         Rearrage();
+    }
+    
+    public void Close()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            RectTransform rect = entries[i].GetComponent<RectTransform>();
+            GameObject entry = entries[i].gameObject;
+            
+            rect.DOAnchorPos(Vector3.zero, 0.3f).SetEase(Ease.OutQuad).SetDelay(0.05f * i).onComplete = delegate
+            {
+                Destroy(entry);
+            };
+        }
+        
+        entries.Clear();
+    }
+
+    public void Toggle()
+    {
+        if (entries.Count == 0)
+        {
+            Open();
+        }
+        else
+        {
+            Close();
+        }
     }
 
     void Rearrage()
@@ -48,7 +77,12 @@ public class RadialMenu : MonoBehaviour
             float x = Mathf.Sin(radiansOfSeperation * i) * radius;
             float y = Mathf.Cos(radiansOfSeperation * i) * radius;
 
-            entries[i].GetComponent<RectTransform>().anchoredPosition = new Vector3(x, y, 0);
+
+            RectTransform rect = entries[i].GetComponent<RectTransform>();
+            
+            rect.localScale = Vector3.zero;
+            rect.DOScale(Vector3.one, 0.3f).SetEase(Ease.OutQuad).SetDelay(0.05f * i);
+            rect.DOAnchorPos(new Vector3(x, y, 0), 0.3f).SetEase(Ease.OutQuad).SetDelay(0.05f * i);
         }
     }
 }
--- STL2 Crab Game/Assets/Scripts/RadialMenuEntry.cs
+++ STL2 Crab Game/Assets/Scripts/RadialMenuEntry.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,21 @@ using TMPro;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 using UnityEngine.EventSystems;
+using DG.Tweening;
 
 public class RadialMenuEntry : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private TextMeshProUGUI label;
     
     [SerializeField] private RawImage icon;
-    
+
+    private RectTransform rect;
+
+    private void Start()
+    {
+        rect = icon.GetComponent<RectTransform>();
+    }
+
     public void SetLabel(string pText)
     {
         label.text = pText;
@@ -34,11 +43,13 @@ public class RadialMenuEntry : MonoBehaviour, IPointerClickHandler, IPointerEnte
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        rect.DOComplete();
+        rect.DOScale(Vector3.one * 1.5f, 0.3f).SetEase(Ease.OutQuad);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        
+        rect.DOComplete();
+        rect.DOScale(Vector3.one, 0.3f).SetEase(Ease.OutQuad);
     }
 }
