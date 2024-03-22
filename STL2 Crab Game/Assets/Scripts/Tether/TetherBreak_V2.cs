b060ec8d0f0b63cab15c0f97f1d71dbc4097ba92
Capt-J-Kirk <creative@jankirk.net>
2024-03-22 11:47:24 +0100

    Tether stuff

--- STL2 Crab Game/Assets/Scripts/Tether/TetherBreak_V2.cs
+++ STL2 Crab Game/Assets/Scripts/Tether/TetherBreak_V2.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Profiling;
 
 public class TetherBreak_V2 : MonoBehaviour
 {
@@ -16,10 +17,17 @@ public class TetherBreak_V2 : MonoBehaviour
     private int boneCount = 11;
     public List<Rigidbody> boneRigidBody = new();
 
+    private List<HingeJoint> hingeJoint = new();
+    private Renderer rend;
+
+    private float maxDist = 5.5f;
+
 
 
     private void Awake()
     {
+        rend = FindChildWithName(transform, "Cylinder").GetComponent<Renderer>();
+
         charLeft = GameObject.FindWithTag("LeftPart");
         charRight = GameObject.FindWithTag("RightPart");
 
@@ -35,23 +43,30 @@ public class TetherBreak_V2 : MonoBehaviour
             if (i == 0)
             {
                 jointLeft = boneRigidBody[i].GetComponent<HingeJoint>();
-
-                //jointLeft.useSpring = true;
-
                 boneRigidBody[i].GetComponent<HingeJoint>().connectedBody = charLeft.GetComponent<Rigidbody>();
-            } 
+                //
+                hingeJoint.Add(jointLeft);
+            }
             else if (i == boneCount - 1)
             {
                 jointRight = boneRigidBody[i].GetComponent<HingeJoint>();
 
                 HingeJoint[] joints = boneRigidBody[i].GetComponents<HingeJoint>();
                 joints[0].connectedBody = boneRigidBody[i - 1];
-                joints[1].connectedBody  = charRight.GetComponent<Rigidbody>();
+                joints[1].connectedBody = charRight.GetComponent<Rigidbody>();
+                //
+                foreach (HingeJoint itr in joints) hingeJoint.Add(itr);
             }
-            else boneRigidBody[i].GetComponent<HingeJoint>().connectedBody = boneRigidBody[i-1];
+            else
+            {
+                HingeJoint hj = boneRigidBody[i].GetComponent<HingeJoint>();
+                hj.connectedBody = boneRigidBody[i - 1];
+                //
+                hingeJoint.Add(hj);
+            }
         }
-      
 
+        foreach (Rigidbody itr in boneRigidBody) itr.mass = 0;
     }
 
 
@@ -65,18 +80,55 @@ public class TetherBreak_V2 : MonoBehaviour
     void Update()
     {
         float dist = Vector3.Distance(charLeft.transform.position, charRight.transform.position);
-        if (dist >= 4)
-        {
-            Debug.Log("Distance is: " + dist);
-        }
 
-        if (dist >= 5.5)
+        //if (dist >= 4) Debug.Log("Distance is: " + dist);
+
+
+        if (dist >= maxDist)
         {
             jointLeft.connectedBody = null;
             jointRight.connectedBody = null;
             Destroy(jointLeft);
             Destroy(jointRight);
             foreach (Rigidbody itr in boneRigidBody) AddMass(itr);
+        }
+
+        //foreach (HingeJoint itr in hingeJoint) StretchControl(itr, 20, 0, 0);
+        Debug.Log("Distance: " + dist);
+        ChangeColor(dist);
+    }
+
+    void ChangeColor(float dist)
+    {
+        float col = 1;
+        if (dist < maxDist) col = dist / maxDist;
+
+        rend.material.color = new Color(col, 0, 0, 1);
+    }
+
+
+    void StretchControl(HingeJoint hingeJoint, float maxStretch, float springForce, float damper)
+    {
+        hingeJoint.useSpring = true;
+
+        float distance = Vector3.Distance(hingeJoint.connectedBody.transform.position, transform.position);
+
+        // If the distance exceeds the maximum stretch, apply spring force
+        if (distance < maxStretch)
+        {
+            JointSpring jointSpring = new JointSpring
+            {
+                spring = springForce,
+                damper = damper
+            };
+            hingeJoint.spring = jointSpring;
+        }
+        else
+        {
+            // If the distance is within the maximum stretch, reset the spring
+            JointSpring jointSpring = new JointSpring();
+            jointSpring.spring = 0;
+            hingeJoint.spring = jointSpring;
         }
     }
 
